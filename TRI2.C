6e4261fc84aa9eff0921cd140c2a97be5586463a
MyreanTrinidad <72651979+MyreanTrinidad@users.noreply.github.com>
2020-10-17 20:09:41 +0800

    Add files via upload

--- TRI2.C
+++ TRI2.C
@@ -1,20 +1,21 @@
 #include<stdio.h>
 
 void main()
+
 {
   int i,j, k;
   clrscr();
 
-  for(i = 0; i < 24; i++);
+  for(i = 0; i < 24; i++)
   {
-     for(j = i; j >= 0; j--);
+     for(j = 24; j >= i; j--)
      {
 	printf(" ");
      }
      for(k = 0; k <= i; k++)
-       printf("*");
-     printf("\n");
-  }
+	 printf("*");
+       printf("\n");
+   }
 
-  getch();
+   getch();
 }
